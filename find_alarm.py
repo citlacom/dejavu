fe5646a7d08d59cb337f3e1862076d6b6ccd76a7
Pablo Cerda <pablo@citla.com>
2016-12-03 22:00:10 +0100

    Use a custom configuration and the yaros-myriam plan video.

--- find_alarm.py
+++ find_alarm.py
@@ -6,7 +6,7 @@ from dejavu import Dejavu
 from dejavu.recognize import FileRecognizer, MicrophoneRecognizer
 
 # load config from a JSON file (or anything outputting a python dictionary)
-with open("dejavu.cnf.SAMPLE") as f:
+with open("dejavu.cnf") as f:
     config = json.load(f)
 
 if __name__ == '__main__':
@@ -16,5 +16,4 @@ if __name__ == '__main__':
 
 	# Or use a recognizer without the shortcut, in anyway you would like
 	recognizer = FileRecognizer(djv)
-	song = recognizer.recognize_file("/Users/pablocc/sample4.wav")
-	print "No shortcut, we recognized: %s\n" % song
+	recognizer.recognize_file("/Users/pablocc/plan_gap_yaros_myriam.wav")
